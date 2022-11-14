04afbd567a97174761bc567bcca750a7af569503
elfish <41020376+garyfishbough@users.noreply.github.com>
2022-11-14 11:01:36 -0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import authRoutes from './routes/auth.js';
+import categoryRoutes from './routes/category.js';
+import productRoutes from './routes/product.js';
+
+dotenv.config();
+
+const app = express();
+
+// db connection
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('DB Connected'))
+    .catch((err) => console.log('DB ERROR => ', err));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// router middleware
+app.use('/api', authRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Node server is running on port ${port}`);
+});
